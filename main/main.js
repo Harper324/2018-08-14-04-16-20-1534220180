39bbe539eee8d825aa3f8d05c679a5a18290a8b0
Harper324 <43511819+Harper324@users.noreply.github.com>
2018-09-25 21:59:18 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,18 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
-};
-
-class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
-
-  minimum() {
-    // Write your code here
-  }
-
-  // Write your code here
+var arr = [12, 15, 7, 9, 7, 11, 20];
+var i, ave, total = arr[0], num = arr.length, min = arr[0], max = arr[0];
+for (i=1; i<arr.length; i++) {
+        total = total + arr[i];
+        if (max < arr[i] {
+                max = arr[i];
+        } else if (min > arr[i]) {
+                min = arr[i];
+        } else {
+                max = max;
+                min = min;
+        }
 }
+ave = total/num;
+console.log('最大值为：' + max);
+console.log('最小值为：' + min);
+console.log('序列中元素个数为：' + num);
+console.log('平均值为：' + ave.toFixed(2));
