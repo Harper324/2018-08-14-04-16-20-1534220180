3fcaadf9c15ed3b3045cd98410ffdf405b33c6fc
cao <caoxi324@foxmail.com>
2018-09-27 14:35:05 +0800

    js2

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ var arr = [6, 9, 15, -2, 92, 11];
 var i, ave, total = arr[0], num = arr.length, min = arr[0], max = arr[0];
 for (i=1; i<arr.length; i++) {
         total = total + arr[i];
-        if (max < arr[i] {
+        if (max < arr[i]) {
                 max = arr[i];
         } else if (min > arr[i]) {
                 min = arr[i];
@@ -12,7 +12,7 @@ for (i=1; i<arr.length; i++) {
         }
 }
 ave = total/num;
-console.log('最小值 =' + min);
-console.log('最大值 =' + max);
-console.log('元素数量 =' + num);
-console.log('平均值 =' + ave.toFixed(2));
+console.log('最小值 = ' + min);
+console.log('最大值 = ' + max);
+console.log('元素数量 = ' + num);
+console.log('平均值 = ' + ave.toFixed(2));
